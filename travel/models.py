2f1f694592c2c7061857305ec0f3fdf5ac536373
Rebaz Pappiland <rebazp@gmail.com>
2024-02-14 11:18:46 +0000

    fixed syntax

--- travel/models.py
+++ travel/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Post(models.model):
-    title = models.Charfield(max_length=250)
+class Post(models.Model):
+    title = models.CharField(max_length=250)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
 
