6b9a5db1ca237f0ca4ba088e81a709c03ffa5a4a
Rebaz Pappiland <rebazp@gmail.com>
2024-02-15 14:30:18 +0000

    setup update post

--- travel/views.py
+++ travel/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm, EditForm
 
 # Create your views here.
 
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
-
 
 class ArticleDetailView(DetailView):
     model = Post
@@ -18,3 +17,8 @@ class AddPostView(CreateView):
     model = Post
     form_class = PostForm
     template_name = 'add_post.html'
+
+class UpdatePostView(UpdateView):
+    model = Post
+    form_class = EditForm
+    template_name = 'update_post.html'
