7168a00bb406dd12c4153fb90a5bd6b3fe096ed1
Rebaz Pappiland <rebazp@gmail.com>
2024-02-15 19:51:01 +0000

    added categorys

--- travel/views.py
+++ travel/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import Post
+from .models import Post, Category
 from .forms import PostForm, EditForm
 from django.urls import reverse_lazy
 # Create your views here.
@@ -28,3 +28,8 @@ class DeletePostView(DeleteView):
     model = Post
     template_name = 'delete_post.html'
     success_url = reverse_lazy('home')
+
+class AddCategoryView(CreateView):
+    model = Category
+    template_name = 'add_category.html'
+    fields = '__all__'
