011ed1e06f16263c2dc1da717ee4351ec7f21eb0
Rebaz Pappiland <rebazp@gmail.com>
2024-02-15 22:51:25 +0000

    add category list navbar

--- travel/views.py
+++ travel/views.py
@@ -3,16 +3,30 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView, D
 from .models import Post, Category
 from .forms import PostForm, EditForm
 from django.urls import reverse_lazy
+
 # Create your views here.
 
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
+    cats = Category.objects.all()
     ordering = ['-post_date']
+
+    def get_context_date(self, *args, **kwargs):
+        cat_menu= Category.objects.all()
+        context = super(HomeView, self).get_context_data(*args, **kwargs)
+        context["cat_menu"] = cat_menu
+        return context
 
 class ArticleDetailView(DetailView):
     model = Post
     template_name = 'article_details.html'
+
+    def get_context_date(self, *args, **kwargs):
+        cat_menu= Category.objects.all()
+        context = super(ArticleDetailView, self).get_context_data(*args, **kwargs)
+        context["cat_menu"] = cat_menu
+        return context
 
 class AddPostView(CreateView):
     model = Post
