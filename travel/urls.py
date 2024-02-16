42b93cc55b14054b131075bfc6284fce233c0ca7
Rebaz Pappiland <rebazp@gmail.com>
2024-02-16 21:42:08 +0000

    added comments

--- travel/urls.py
+++ travel/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView, AddCategoryView, CategoryView, LikeView, AddCommentView
 
 # Urls for the project
+# The code in this file is inspired from:
+# Code Institute, I think therefore i blog project
+# Youtube series Django Tutorial by [Net Ninja](https://www.youtube.com/watch?v=n-FTlQ7Djqc&list=PL4cUxeGkcC9ib4HsrXEYpQnTOTZE1x0uc&index=1&ab_channel=NetNinja)
+# Youtube series Python Django Tutorial by [Corey Schafer](https://www.youtube.com/watch?v=UmljXZIypDc&list=PL-osiE80TeTtoQCKZ03TU5fNfx2UY6U4p&index=1&ab_channel=CoreySchafer)
+# Yoube Python Django Web Framework by [FreeCodeCamp](https://www.youtube.com/watch?v=F5mRW0jo-U4&ab_channel=freeCodeCamp.org)
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
