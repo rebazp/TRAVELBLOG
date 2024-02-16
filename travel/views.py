7b83eea0483fde260689be118866b1c32cef8704
Rebaz Pappiland <rebazp@gmail.com>
2024-02-16 12:35:56 +0000

    install category navbar

--- travel/views.py
+++ travel/views.py
@@ -12,7 +12,7 @@ class HomeView(ListView):
     cats = Category.objects.all()
     ordering = ['-post_date']
 
-    def get_context_date(self, *args, **kwargs):
+    def get_context_data(self, *args, **kwargs):
         cat_menu= Category.objects.all()
         context = super(HomeView, self).get_context_data(*args, **kwargs)
         context["cat_menu"] = cat_menu
