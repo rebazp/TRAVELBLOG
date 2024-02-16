d53b35686fffe7ee9757d00b0e3e815a25ab992c
Rebaz Pappiland <rebazp@gmail.com>
2024-02-16 15:44:35 +0000

    setup number of likes

--- travel/views.py
+++ travel/views.py
@@ -23,10 +23,13 @@ class ArticleDetailView(DetailView):
     model = Post
     template_name = 'article_details.html'
 
-    def get_context_date(self, *args, **kwargs):
+    def get_context_data(self, *args, **kwargs):
         cat_menu= Category.objects.all()
         context = super(ArticleDetailView, self).get_context_data(*args, **kwargs)
+        thumb = get_object_or_404(Post, id=self.kwargs['pk'])
+        total_likes = thumb.total_likes()
         context["cat_menu"] = cat_menu
+        context["total_likes"] = total_likes
         return context
 
 class AddPostView(CreateView):
